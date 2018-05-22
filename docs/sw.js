397dd7c909feba5522c3fa72ba4396584289ddd4
imouou <imouou@gmail.com>
2018-05-22 18:01:58 +0800

    更新缓存

--- docs/sw.js
+++ docs/sw.js
@@ -6,7 +6,7 @@
  * Register service worker.
  * ========================================================== */
 
-const RUNTIME = 'bui-guide2'
+const RUNTIME = 'bui-guide3'
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
   'fonts.gstatic.com',
